ed452178c65280296787606d6a3cd6cd9e4f9535
Ivan Zlatev <i.zlatev@marketinvoice.com>
2014-10-24 22:22:00 +0100

    Expose the Assembly Name a specification belongs to as part of the SpecificationInfo which Listeners consume. This aids non-sequential runners to collect information.

--- Source/Machine.Specifications/Runner/SpecificationInfo.cs
+++ Source/Machine.Specifications/Runner/SpecificationInfo.cs
@@ -5,18 +5,20 @@ namespace Machine.Specifications.Runner
   [Serializable]
   public class SpecificationInfo
   {
-    public SpecificationInfo(string leader, string name, string containingType, string fieldName)
+    public SpecificationInfo(string leader, string name, string containingType, string fieldName, string assemblyName)
     {
       Leader = leader;
       Name = name;
       ContainingType = containingType;
       FieldName = fieldName;
+      AssemblyName = assemblyName;
     }
 
     public string Leader { get; set; }
     public string Name { get; set; }
     public string ContainingType { get; set; }
     public string FieldName { get; set; }
+    public string AssemblyName { get; set; }
     public string CapturedOutput { get; set; }
   }
 }
